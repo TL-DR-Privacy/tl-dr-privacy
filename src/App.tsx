4a5c08f0dbd7fe7d21dc96347c03b4812dde155c
marktmaloney <markmaloney03@yahoo.com>
2025-03-13 16:23:24 -0500

    updated comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-// App.tsx
+// App.tsx Prologue Comments
 // This component serves as the main hub for navigating between different pages of the app.
 // Site, history, search and settings can be toggled between using the navbar
 // Programmers: Nayyir and Mark
@@ -7,9 +7,9 @@
 // Preconditions: React development environment is set up. Site, History, Search and Setting exist in the correct path. App.css is availible for styling.
 // Postconditions: The component renders the content of the page corresponding to the activePage state.
 // Error and Exceptions: If activePage value is invalid, Site is the default component.
-// Side effects:
-// Invariants:
-// Known Faults: 
+// Side effects: None
+// Invariants: None
+// Known Faults: None
 
 import { useEffect, useState } from "react";
 import "./App.css";
@@ -24,14 +24,16 @@ export default function App() {
   const [siteName, setSiteName] = useState("");
   const [fontSize, setFontSize] = useState(25);
 
-  useEffect(() => { //get name of site the user is currently on
+  useEffect(() => {
+    //get name of site the user is currently on
     if (activePage === "site") {
       chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         const url = new URL(tabs[0].url || "");
         setSiteName(url.hostname);
 
-        const length = url.hostname.length; 
-        if (length > 14) { //change font size of site page's title depending on length of url to avoid overflow
+        const length = url.hostname.length;
+        if (length > 14) {
+          //change font size of site page's title depending on length of url to avoid overflow
           setFontSize(20);
         } else {
           setFontSize(25);
@@ -58,8 +60,8 @@ export default function App() {
   };
 
   //render page title in header section depending on which page open
-  const renderPageTitle = () => { 
-    switch(activePage) {
+  const renderPageTitle = () => {
+    switch (activePage) {
       case "site":
         return `Site: ${siteName}`;
       case "history":
@@ -71,19 +73,20 @@ export default function App() {
       default:
         return "";
     }
-  }
+  };
 
   return (
     <div className="app-container">
       <div className="header-section">
         <div className="logo-container">
-          <img src="128.png" alt="TLDR Privacy Logo" className="app-logo"/> {/*adds logo to top*/}
+          <img src="128.png" alt="TLDR Privacy Logo" className="app-logo" />{" "}
+          {/*adds logo to top*/}
         </div>
         {/*if the page is site then it uses the site-title css class and changes font size depending on url length
           else it uses the normal page-title format and font size */}
         <h2
-          className = {`page-title ${activePage === "site" ? "site-title" : ""}`} 
-          style = {{ fontSize: activePage === "site" ? `${fontSize}px` : "25px" }}
+          className={`page-title ${activePage === "site" ? "site-title" : ""}`}
+          style={{ fontSize: activePage === "site" ? `${fontSize}px` : "25px" }}
         >
           {renderPageTitle()}
         </h2>
