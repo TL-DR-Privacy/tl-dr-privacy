dd1ce8d696f402f7ccc37faa95856aa7104e7228
dwesty23 <120140940+dwesty23@users.noreply.github.com>
2025-03-27 16:28:52 -0500

    getting rid of this fr

--- main.js
+++ main.js
@@ -27,7 +27,6 @@
  * Invariants: None
  * Any known faults: None
  *********************************************************/
-import fs from 'fs';
 import { getWebsiteInput, generateFilename, uploadToS3, getExistingPolicy} from './helpers.js';
 import { findPrivacyPolicy } from './findPolicy.js';
 import { extractPolicyText } from './crawler.js';
